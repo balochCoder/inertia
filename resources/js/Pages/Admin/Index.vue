3ba1a2301d77583e597d614aadcd096245797c53
balochCoder <raheelahmed017@gmail.com>
2024-09-08 02:35:21 +0500

    Initial Commit

---
+++ resources/js/Pages/Admin/Index.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AdminAuthenticatedLayout from '@/Layouts/AdminAuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Admin" />
+
+    <AdminAuthenticatedLayout>
+       <div class="py-4">
+           <h1>Welcome Admin</h1>
+       </div>
+
+    </AdminAuthenticatedLayout>
+</template>
